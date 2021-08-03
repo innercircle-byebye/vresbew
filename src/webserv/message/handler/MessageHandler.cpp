afb34db25dc80522a654f5aa9552c73e85f20b23
Sungyong Cho <to.sungyongcho@gmail.com>
2021-08-03 17:20:40 +0900

    MessageHandler 함수 통합

--- src/webserv/message/handler/MessageHandler.cpp
+++ src/webserv/message/handler/MessageHandler.cpp
@@ -26,6 +26,8 @@ void MessageHandler::check_request_header(Connection *c) {
   // 있어야되는지??
   // request_handler_.setRequest(&c->getRequest());
 
+
+
   if (request_handler_.isHostHeaderExist() == false) {
     c->status_code_ = 400;
     c->setRecvPhase(MESSAGE_BODY_COMPLETE);
@@ -37,26 +39,25 @@ void MessageHandler::check_request_header(Connection *c) {
     c->setRecvPhase(MESSAGE_BODY_COMPLETE);
     return;
   }
-}
 
-void MessageHandler::check_cgi_request(Connection *c) {
-  ServerConfig *serverconfig_test = c->getHttpConfig()->getServerConfig(c->getSockaddrToConnect().sin_port, c->getSockaddrToConnect().sin_addr.s_addr, c->getRequest().getHeaderValue("Host"));
-  LocationConfig *locationconfig_test = serverconfig_test->getLocationConfig(c->getRequest().getPath());
+  if (!c->getRequest().getHeaderValue("Content-Length").empty())
+    c->setStringBufferContentLength(stoi(c->getRequest().getHeaderValue("Content-Length")));
+
+  if (c->interrupted == true) {
+    c->setRecvPhase(MESSAGE_INTERRUPTED);
+    return;
+  }
+
   //TODO: c->getRequest().getUri().find_last_of() 부분을 메세지 헤더의 mime_types로 확인하도록 교체/ 확인 필요
   if (!locationconfig_test->getCgiPath().empty() &&
       locationconfig_test->checkCgiExtension(c->getRequest().getPath())) {
-    c->setRecvPhase(MESSAGE_CGI_PROCESS);
+    CgiHandler::init_cgi_child(c);
+  } else {
+    if (c->getStringBufferContentLength() == 0)
+      c->setRecvPhase(MESSAGE_BODY_COMPLETE);
+    else
+      c->setRecvPhase(MESSAGE_BODY_INCOMING);
   }
-}
-
-void MessageHandler::check_body_status(Connection *c) {
-  //TODO: 아래 주석에 나와있는 조건문으로 변경
-  // if (c->getStringBufferContentLength() == 0
-  //  && !c->getRequest().getHeaderValue("Content-Length").empty())
-  if (c->getStringBufferContentLength() == 0)
-    c->setRecvPhase(MESSAGE_BODY_COMPLETE);
-  else
-    c->setRecvPhase(MESSAGE_BODY_INCOMING);
 }
 
 void MessageHandler::handle_request_body(Connection *c) {
@@ -118,28 +119,6 @@ void MessageHandler::executePutMethod(std::string path, std::string content) {
   output << content;
   output.close();
 }
-
-// bool MessageHandler::isValidRequestMethod(const std::string &method) {
-//   if (method.compare("GET") && method.compare("POST") &&
-//       method.compare("DELETE") && method.compare("PUT") && method.compare("HEAD"))
-//     return (false);
-//   return (true);
-// }
-
-// bool MessageHandler::isValidRequestVersion(const std::string &http_version, const std::map<std::string, std::string> &headers) {
-//   if (!http_version.compare("HTTP/1.1")) {
-//     if (headers.count("Host"))
-//       return (true);
-//     return (false);
-//   } else if (!http_version.compare("HTTP/1.0")) {
-//     // if (!headers.count("Host"))
-//     //   request_->setHeader("Host", "");   // ?? 자동으로 ""되어있는거 아닌지?
-//     // A: 아닙니다. Response의 경우 응답 헤더들을 미리 설정 해 놓았지만 Request는 어떤 헤더가 들어 올지 모르기 때문에 설정 해주지 않았습니다.
-//     // HTTP/1.0 처리 하지 못하는 상황 발생하여 수정하겠습니다.
-//     return (true);
-//   }
-//   return (false);
-// }
 
 void MessageHandler::check_interrupt_received(Connection *c) {
   int i = 0;
--- src/webserv/message/handler/MessageHandler.hpp
+++ src/webserv/message/handler/MessageHandler.hpp
@@ -9,6 +9,7 @@
 #include <string>
 
 #include "webserv/config/HttpConfig.hpp"
+#include "webserv/message/handler/CgiHandler.hpp"
 #include "webserv/message/handler/RequestHandler.hpp"
 #include "webserv/message/handler/ResponseHandler.hpp"
 #include "webserv/socket/Connection.hpp"
@@ -23,13 +24,12 @@ class MessageHandler {
  public:
   static ResponseHandler response_handler_;
   static RequestHandler request_handler_;
+
   static void handle_request_header(Connection *c);
   static void check_request_header(Connection *c);
-  static void check_cgi_request(Connection *c);
-  static void check_body_status(Connection *c);
   static void handle_request_body(Connection *c);
-  static void handle_response(Connection *c);
 
+  static void handle_response(Connection *c);
   static void set_response_header(Connection *c);
   static void set_response_message(Connection *c);
   static void send_response_to_client(Connection *c);
@@ -38,8 +38,6 @@ class MessageHandler {
 
  private:
   static void executePutMethod(std::string path, std::string content);
-  // static bool isValidRequestMethod(const std::string &method);
-  // static bool isValidRequestVersion(const std::string &http_version, const std::map<std::string, std::string> &headers);
 };
 }  // namespace ft
 #endif

af3f9ead2df97c9e06e14272551e05b1c371bede
강명구 <mg5566@hanmail.net>
2021-08-12 18:24:18 +0900

    setting delete method

--- src/webserv/message/handler/ResponseHandler.cpp
+++ src/webserv/message/handler/ResponseHandler.cpp
@@ -28,7 +28,7 @@ void ResponseHandler::executeMethod(Request &request) {
   else if (request.getMethod() == "POST")
     processPostMethod(request, location);
   else if (request.getMethod() == "DELETE")
-    processDeleteMethod(request.getFilePath());
+    processDeleteMethod(request);
 }
 
 void ResponseHandler::setDefaultHeader(Connection *c, Request &request) {
@@ -200,9 +200,21 @@ void ResponseHandler::processPostMethod(Request &request, LocationConfig *&locat
   setStatusLineWithCode(200);
 }
 
-void ResponseHandler::processDeleteMethod(const std::string &filepath) {
-  if (!filepath.compare("/")) {  // URI 에 "/" 만 있는 경우
-    if (stat(filepath.c_str(), &this->stat_buffer_) < 0) {
+void ResponseHandler::processDeleteMethod(Request &request) {
+  // test print log
+  std::cout << "test print resquest path" << std::endl;
+  std::cout << "getPath() : " << request.getPath() << std::endl;
+  std::cout << "getFilePath() : " << request.getFilePath() << std::endl;
+  // var/www/html/index.html 이 출력되고 있습니다... index.html 이 사라졌으면 좋겠음.
+  std::cout << "compare(\"/\") : " << request.getPath().compare("/") << std::endl;
+  // if (!filepath.compare("/")) {  // URI 에 "/" 만 있는 경우
+  // if (stat(filepath.c_str(), &this->stat_buffer_) < 0) {
+  if (!request.getPath().compare("/")) {  // URI 에 "/" 만 있는 경우
+    std::string path = request.getFilePath().substr(0, request.getFilePath().find_last_of("/") + 1);
+    // var/www/html/
+    std::cout << "path : " << path << std::endl;
+    // if (stat(request.getFilePath().c_str(), &this->stat_buffer_) < 0) {
+    if (stat(path.c_str(), &this->stat_buffer_) < 0) {
       setStatusLineWithCode(405);
       return;
     } else {
@@ -210,14 +222,16 @@ void ResponseHandler::processDeleteMethod(const std::string &filepath) {
         DIR *dir_ptr;
         struct dirent *item;
 
-        if (!(dir_ptr = opendir(filepath.c_str()))) {
+        // if (!(dir_ptr = opendir(filepath.c_str()))) {
+        if (!(dir_ptr = opendir(path.c_str()))) {
           setStatusLineWithCode(403);  // Not Allowed
           return;
         }
         while ((item = readdir(dir_ptr))) {
           if (strcmp(item->d_name, ".") == 0 || strcmp(item->d_name, "..") == 0)
             continue;
-          std::string new_path(filepath);
+          // std::string new_path(filepath);
+          std::string new_path(path);
           new_path += item->d_name;
           if (deletePathRecursive(new_path) == -1) {
             setStatusLineWithCode(403);
@@ -226,7 +240,8 @@ void ResponseHandler::processDeleteMethod(const std::string &filepath) {
         }
         setStatusLineWithCode(403);
       } else {
-        if (remove(filepath.c_str()) != 0) {
+        // if (remove(filepath.c_str()) != 0) {
+        if (remove(path.c_str()) != 0) {
           setStatusLineWithCode(403);
           return;
         }
@@ -234,19 +249,24 @@ void ResponseHandler::processDeleteMethod(const std::string &filepath) {
       }
     }
   } else {  // "/" 가 아닌 경우
-    if (stat(filepath.c_str(), &this->stat_buffer_) < 0) {
+    std::string path = request.getFilePath();
+    if (stat(path.c_str(), &this->stat_buffer_) < 0) {
       setStatusLineWithCode(404);
     } else {
       // file or directory
       if (S_ISDIR(this->stat_buffer_.st_mode)) {
-        setStatusLineWithCode(409);
-      } else {  // is not directory == file ?!
-        if (remove(filepath.c_str()) != 0) {
+        if (deletePathRecursive(path) == -1) {
           setStatusLineWithCode(403);
           return;
         }
-        setStatusLineWithCode(204);
+      } else {  // is not directory == file ?!
+        // if (remove(filepath.c_str()) != 0) {
+        if (remove(path.c_str()) != 0) {
+          setStatusLineWithCode(403);
+          return;
+        }
       }
+      setStatusLineWithCode(204);
     }
   }
 }

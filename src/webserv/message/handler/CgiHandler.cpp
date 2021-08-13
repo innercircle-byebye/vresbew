1ad23f034f86a5e2c57059ed1bbfe7a254e6a7fe
Sungyong Cho <to.sungyongcho@gmail.com>
2021-08-14 02:21:46 +0900

    final fix with no comment

--- src/webserv/message/handler/CgiHandler.cpp
+++ src/webserv/message/handler/CgiHandler.cpp
@@ -258,10 +258,6 @@ void CgiHandler::setupCgiMessage(Connection *c) {
   c->getResponse().setHeader("Content-Length", SSTR(c->temp.size()));
   MessageHandler::response_handler_.makeResponseHeader();
 
-  std::cout << "===========header=========" << std::endl;
-  std::cout << c->getResponse().getHeaderMsg() << std::endl;
-  std::cout << "===========header=========" << std::endl;
-
   if (!c->temp.empty())
     c->getResponse().getHeaderMsg().append(c->temp);
 
